0d8bc911ede6122dbef48f095f5255d08f25b52f
pv-kiran <kiranpv903@gmail.com>
2024-11-05 12:00:26 +0530

    resend otp logic added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,11 +5,13 @@ const {
   googleSignIn,
   userAccountCreation,
   userLogout,
+  useResendOtp,
 } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/auth/signin", userSignup);
 router.post("/auth/otp/verify", userOtpVerify);
+router.post("/auth/otp/resend", useResendOtp);
 router.post("/auth/google/signin", googleSignIn);
 router.put("/account/setup/complete/:userId", userAccountCreation);
 router.get("/auth/signout", userLogout);
--- utils/validationHelper.js
+++ utils/validationHelper.js
@@ -1,269 +1,258 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const validateAdminSignup = Joi.object({
-    name: Joi.string()
-        .min(4)
-        .max(40)
-        .required()
-        .messages({
-            "any.required": "Name is required",
-        }),
-    email: Joi.string()
-        .email({ minDomainSegments: 2})
-        .required()
-        .messages({
-            "any.required": "Email is required",
-            "string.email": "Invalid email format",
-        }),
-    password: Joi.string()
-        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,30}$'))
-        .required()
-        .messages({
-            "any.required": "Password is required",
-            "string.pattern.base": "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character",
-        }),        
-})
+  name: Joi.string().min(4).max(40).required().messages({
+    "any.required": "Name is required",
+  }),
+  email: Joi.string().email({ minDomainSegments: 2 }).required().messages({
+    "any.required": "Email is required",
+    "string.email": "Invalid email format",
+  }),
+  password: Joi.string()
+    .pattern(
+      new RegExp(
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,30}$"
+      )
+    )
+    .required()
+    .messages({
+      "any.required": "Password is required",
+      "string.pattern.base":
+        "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character",
+    }),
+});
 
 const validateAdminLogin = Joi.object({
-    email: Joi.string()
-        .email({ minDomainSegments: 2})
-        .required()
-        .messages({
-            "any.required": "Email is required",
-            "string.email": "Invalid email format",
-        }),
-    password: Joi.string()
-        .required()
-        .messages({
-            "any.required": "Password is required",
-        })
-})
+  email: Joi.string().email({ minDomainSegments: 2 }).required().messages({
+    "any.required": "Email is required",
+    "string.email": "Invalid email format",
+  }),
+  password: Joi.string().required().messages({
+    "any.required": "Password is required",
+  }),
+});
+
+const validateEmail = Joi.object({
+  email: Joi.string().email({ minDomainSegments: 2 }).required().messages({
+    "any.required": "Email is required",
+    "string.email": "Invalid email format",
+  }),
+});
 
 const validateOtp = Joi.object({
-    email: Joi.string()
-        .email()
-        .required()
-        .messages({
-            'string.empty': 'Email is required',
-            'string.email': 'Invalid email format'
-        }),
-    otp: Joi.number()
-        .integer()
-        .min(100000)
-        .max(999999)
-        .required()
-        .messages({
-            'number.base': 'OTP must be a number',
-            'number.min': 'OTP must be a 6-digit number',
-            'number.max': 'OTP must be a 6-digit number',
-            'any.required': 'OTP is required'
-        })
-})
+  email: Joi.string().email().required().messages({
+    "string.empty": "Email is required",
+    "string.email": "Invalid email format",
+  }),
+  otp: Joi.number().integer().min(100000).max(999999).required().messages({
+    "number.base": "OTP must be a number",
+    "number.min": "OTP must be a 6-digit number",
+    "number.max": "OTP must be a 6-digit number",
+    "any.required": "OTP is required",
+  }),
+});
 
 const validateCategory = Joi.object({
-    categoryName: Joi.string()
-        .min(3)
-        .max(30)
-        .required()
-        .messages({
-            'string.base': 'Category name should be a string',
-            'string.empty': 'Category name cannot be empty',
-            'string.min': 'Category name should have a minimum length of 3',
-            'string.max': 'Category name should have a maximum length of 30',
-            'any.required': 'Category name is required',
-        }),
-})
+  categoryName: Joi.string().min(3).max(30).required().messages({
+    "string.base": "Category name should be a string",
+    "string.empty": "Category name cannot be empty",
+    "string.min": "Category name should have a minimum length of 3",
+    "string.max": "Category name should have a maximum length of 30",
+    "any.required": "Category name is required",
+  }),
+});
 
 const validateHomestay = Joi.object({
-    title: Joi.string().required().messages({
-        'any.required': 'Title is required.',
-        'string.base': 'Title should be a string',
-        'string.empty': 'Title cannot be empty',
-        'string.min': 'Title should have a minimum length of 3',
+  title: Joi.string().required().messages({
+    "any.required": "Title is required.",
+    "string.base": "Title should be a string",
+    "string.empty": "Title cannot be empty",
+    "string.min": "Title should have a minimum length of 3",
+  }),
+  description: Joi.string().required().messages({
+    "any.required": "Description is required.",
+    "string.base": "Description should be a string",
+    "string.empty": "Description cannot be empty",
+    "string.min": "Description should have a minimum length of 3",
+  }),
+  address: Joi.object({
+    street: Joi.string().required().messages({
+      "any.required": "Street address is required.",
+      "string.empty": "Street address cannot be empty",
     }),
-    description: Joi.string().required().messages({
-        'any.required': 'Description is required.',
-        'string.base': 'Description should be a string',
-        'string.empty': 'Description cannot be empty',
-        'string.min': 'Description should have a minimum length of 3',
+    city: Joi.string().required().messages({
+      "any.required": "City is required.",
+      "string.empty": "City cannot be empty",
     }),
-    address: Joi.object({
-        street: Joi.string().required().messages({
-            'any.required': 'Street address is required.',
-            'string.empty': 'Street address cannot be empty',
-        }),
-        city: Joi.string().required().messages({
-            'any.required': 'City is required.',
-            'string.empty': 'City cannot be empty',
-        }),
-        district: Joi.string().required().messages({
-            'any.required': 'District is required.',
-            'string.empty': 'District cannot be empty',
-        }),
-        state: Joi.string().required().messages({
-            'any.required': 'State is required.',
-            'string.empty': 'State cannot be empty',
-        }),
-        zip: Joi.string().required().messages({
-            'any.required': 'ZIP code is required.',
-            'string.empty': 'ZIP code cannot be empty',
-        }),
-        coordinates: Joi.object({
-            latitude: Joi.number().required().messages({
-                'number.base': 'Latitude must be a number.',
-                'any.required': 'Latitude is required.'
-            }),
-            longitude: Joi.number().required().messages({
-                'number.base': 'Longitude must be a number.',
-                'any.required': 'Longitude is required.'
-            }),
-        }).required().messages({
-            'any.required': 'Coordinates are required.'
-        }),
-    }).required().messages({
-        'any.required': 'Address is required.'
+    district: Joi.string().required().messages({
+      "any.required": "District is required.",
+      "string.empty": "District cannot be empty",
     }),
-    amenities: Joi.array().items(
-        Joi.object({
-            title: Joi.string().required(),
-            icon: Joi.string().uri().optional()
-        })
-    ).required().messages({
-        'any.required': 'Amenities are required.',
+    state: Joi.string().required().messages({
+      "any.required": "State is required.",
+      "string.empty": "State cannot be empty",
     }),
-    noOfRooms: Joi.number().integer().required().messages({
-        'number.base': 'Number of rooms must be a number.',
-        'any.required': 'Number of rooms is required.'
+    zip: Joi.string().required().messages({
+      "any.required": "ZIP code is required.",
+      "string.empty": "ZIP code cannot be empty",
     }),
-    noOfBathRooms: Joi.number().integer().required().messages({
-        'number.base': 'Number of bathrooms must be a number.',
-        'any.required': 'Number of bathrooms is required.'
+    coordinates: Joi.object({
+      latitude: Joi.number().required().messages({
+        "number.base": "Latitude must be a number.",
+        "any.required": "Latitude is required.",
+      }),
+      longitude: Joi.number().required().messages({
+        "number.base": "Longitude must be a number.",
+        "any.required": "Longitude is required.",
+      }),
+    })
+      .required()
+      .messages({
+        "any.required": "Coordinates are required.",
+      }),
+  })
+    .required()
+    .messages({
+      "any.required": "Address is required.",
     }),
-    pricePerNight: Joi.number().required().messages({
-        'number.base': 'Price per night must be a number.',
-        'any.required': 'Price per night is required.'
+  amenities: Joi.array()
+    .items(
+      Joi.object({
+        title: Joi.string().required(),
+        icon: Joi.string().uri().optional(),
+      })
+    )
+    .required()
+    .messages({
+      "any.required": "Amenities are required.",
     }),
-    maxGuests: Joi.number().integer().required().min(1).messages({
-        'number.base': 'Maximum number of guests must be a number.',
-        'any.required': 'Maximum number of guests is required.'
+  noOfRooms: Joi.number().integer().required().messages({
+    "number.base": "Number of rooms must be a number.",
+    "any.required": "Number of rooms is required.",
+  }),
+  noOfBathRooms: Joi.number().integer().required().messages({
+    "number.base": "Number of bathrooms must be a number.",
+    "any.required": "Number of bathrooms is required.",
+  }),
+  pricePerNight: Joi.number().required().messages({
+    "number.base": "Price per night must be a number.",
+    "any.required": "Price per night is required.",
+  }),
+  maxGuests: Joi.number().integer().required().min(1).messages({
+    "number.base": "Maximum number of guests must be a number.",
+    "any.required": "Maximum number of guests is required.",
+  }),
+  // images: Joi.array().items(Joi.string()).optional(),
+  hotelPolicies: Joi.object({
+    checkInTime: Joi.string().required().messages({
+      "string.empty": "Check-in time is required.",
+      "any.required": "Check-in time is required.",
     }),
-    // images: Joi.array().items(Joi.string()).optional(),
-    hotelPolicies: Joi.object({
-        checkInTime: Joi.string().required().messages({
-            'string.empty': 'Check-in time is required.',
-            'any.required': 'Check-in time is required.'
-        }),
-        checkOutTime: Joi.string().required().messages({
-            'string.empty': 'Check-out time is required.',
-            'any.required': 'Check-out time is required.'
-        }),
-        guestPolicies: Joi.array().items(Joi.string()).optional()
-    }).required().messages({
-        'any.required': 'Hotel policies are required.'
+    checkOutTime: Joi.string().required().messages({
+      "string.empty": "Check-out time is required.",
+      "any.required": "Check-out time is required.",
     }),
-    categoryId: Joi.string()
-        .required()
-        .custom((value, helpers) => {
-            // Check if value is a valid MongoDB ObjectId
-            if (!mongoose.Types.ObjectId.isValid(value)) {
-                return helpers.message("Invalid Category ID format.");
-            }
-            return value; 
-        })
-        .messages({
-            'any.required': 'Category is required.',
-            'string.empty': 'Category is required.',
-        })
-})
+    guestPolicies: Joi.array().items(Joi.string()).optional(),
+  })
+    .required()
+    .messages({
+      "any.required": "Hotel policies are required.",
+    }),
+  categoryId: Joi.string()
+    .required()
+    .custom((value, helpers) => {
+      // Check if value is a valid MongoDB ObjectId
+      if (!mongoose.Types.ObjectId.isValid(value)) {
+        return helpers.message("Invalid Category ID format.");
+      }
+      return value;
+    })
+    .messages({
+      "any.required": "Category is required.",
+      "string.empty": "Category is required.",
+    }),
+});
 
 const validateUserSignup = Joi.object({
-    email: Joi.string()
-        .email({ minDomainSegments: 2})
-        .required()
-        .messages({
-            "any.required": "Email is required",
-            "string.email": "Invalid email format",
-        })       
-})
-
+  email: Joi.string().email({ minDomainSegments: 2 }).required().messages({
+    "any.required": "Email is required",
+    "string.email": "Invalid email format",
+  }),
+});
 
 const is18OrOlder = (dob) => {
-    const today = new Date();
-    const dobDate = new Date(dob);
-    const age = today.getFullYear() - dobDate.getFullYear();
-    const monthDifference = today.getMonth() - dobDate.getMonth();
+  const today = new Date();
+  const dobDate = new Date(dob);
+  const age = today.getFullYear() - dobDate.getFullYear();
+  const monthDifference = today.getMonth() - dobDate.getMonth();
 
-    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dobDate.getDate())) {
-        return age - 1;
-    }
-    return age;
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < dobDate.getDate())
+  ) {
+    return age - 1;
+  }
+  return age;
 };
 
-
 const userValidationSchema = Joi.object({
-    fullName: Joi.string()
-        .min(2)
-        .max(100)
-        .required()
-        .messages({
-            "any.required": "Full name is required",
-            "string.empty": "Full name cannot be empty",
-            "string.min": "Full name should have a minimum length of 2 characters",
-            "string.max": "Full name should have a maximum length of 100 characters",
-            "string.base": "Full name must be a string"
-        }),
+  fullName: Joi.string().min(2).max(100).required().messages({
+    "any.required": "Full name is required",
+    "string.empty": "Full name cannot be empty",
+    "string.min": "Full name should have a minimum length of 2 characters",
+    "string.max": "Full name should have a maximum length of 100 characters",
+    "string.base": "Full name must be a string",
+  }),
 
-    email: Joi.string()
-        .email({ minDomainSegments: 2 })
-        .required()
-        .messages({
-            "any.required": "Email is required",
-            "string.email": "Invalid email format",
-            "string.empty": "Email cannot be empty",
-            "string.base": "Email must be a string"
-        }),
+  email: Joi.string().email({ minDomainSegments: 2 }).required().messages({
+    "any.required": "Email is required",
+    "string.email": "Invalid email format",
+    "string.empty": "Email cannot be empty",
+    "string.base": "Email must be a string",
+  }),
 
-    dob: Joi.date()
-        .iso()
-        .required()
-        .custom((value, helpers) => {
-            if (is18OrOlder(value) < 18) {
-                return helpers.message("User must be at least 18 years old");
-            }
-            return value;
-        })
-        .messages({
-            "any.required": "Date of birth is required",
-            "date.base": "Date of birth must be a valid date",
-            "date.format": "Date of birth must be in ISO format (YYYY-MM-DD)"
-        }),
+  dob: Joi.date()
+    .iso()
+    .required()
+    .custom((value, helpers) => {
+      if (is18OrOlder(value) < 18) {
+        return helpers.message("User must be at least 18 years old");
+      }
+      return value;
+    })
+    .messages({
+      "any.required": "Date of birth is required",
+      "date.base": "Date of birth must be a valid date",
+      "date.format": "Date of birth must be in ISO format (YYYY-MM-DD)",
+    }),
 
-    isMarketingAllowed: Joi.boolean()
-        .messages({
-            "boolean.base": "Marketing preference must be a boolean value (true or false)"
-        })
-})
+  isMarketingAllowed: Joi.boolean().messages({
+    "boolean.base":
+      "Marketing preference must be a boolean value (true or false)",
+  }),
+});
 
 // Custom ObjectId validation rule
 const objectIdValidation = (value, helpers) => {
-    if (!mongoose.isValidObjectId(value)) {
-        return helpers.message('Invalid ID format');
-    }
-    return value;
+  if (!mongoose.isValidObjectId(value)) {
+    return helpers.message("Invalid ID format");
+  }
+  return value;
 };
 
 const validateHomestayId = Joi.object({
-    homestayId: Joi.string().custom(objectIdValidation).required()
+  homestayId: Joi.string().custom(objectIdValidation).required(),
 });
 
 module.exports = {
-    validateAdminSignup,
-    validateAdminLogin,
-    validateOtp,
-    validateCategory,
-    validateHomestay,
-    validateUserSignup,
-    userValidationSchema,
-    validateHomestayId
-}
+  validateAdminSignup,
+  validateAdminLogin,
+  validateOtp,
+  validateCategory,
+  validateHomestay,
+  validateUserSignup,
+  userValidationSchema,
+  validateHomestayId,
+  validateEmail,
+};
