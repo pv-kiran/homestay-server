a3df2483aa2eed731ea07237aa6d53250721140b
silpasheela <silpasheelassk@gmail.com>
2024-11-16 13:31:21 +0530

    #30 - auth middleware for isadmin and isuser added

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const Admin = require("../models/admin");
+const User = require("../models/user");
 const {verifyToken} = require('../utils/jwtHelper');
 
 
@@ -22,6 +23,47 @@ const authenticateToken = (req,res,next) => {
     }
 }
 
+
+const isAdmin = async(req, res, next) => {
+    try {
+        const admin = await Admin.findById({_id:req.userId});
+        if(admin?.role === 'admin') {
+            next();
+        }
+        else {
+            return res.status(400).json({
+                success: false,
+                message: 'Unauthorized access'
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal Server error"
+        })
+    }
+}
+
+const isUser = async(req, res, next) => {
+    try {
+        const user = await User.findById({_id:req.userId});
+        if(!user?.isDisabled) {
+            next();
+        }
+        else {
+            return res.status(400).json({
+                success: false,
+                message: 'Your account has been temporarily suspended! Please contact admin'
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal Server error"
+        })
+    }
+}
+
 module.exports = {
     authenticateToken,
+    isAdmin,
+    isUser,
 }
